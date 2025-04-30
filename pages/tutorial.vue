7d1fa5df4f3e25f2fbc8f3a8729584b4f858ab0c
Aditya Muhammad Zaini <adityamzainir@gmail.com>
2025-04-30 10:47:57 +0700

    fix(tutorial): add classes

--- pages/tutorial.vue
+++ pages/tutorial.vue
@@ -5,7 +5,7 @@
         <Element src="/element/home.png" customClass="h-6" />
       </CircleButton>
     </div>
-    <div v-show="currentPage > 0 && currentPage < 14" class="h-16 w-16 md:w-24 absolute top-1/2 -translate-y-1/2 left-0 flex justify-evenly items-center">
+    <div v-show="currentPage > 0 && currentPage < 15" class="h-16 w-16 md:w-24 absolute top-1/2 -translate-y-1/2 left-0 flex justify-evenly items-center">
       <CircleButton @click="handleClick('back')" class="h-12 w-12" variant="secondary">
         <Element src="/element/arrow-left.png" customClass="h-6" />
       </CircleButton>
@@ -20,7 +20,7 @@
     </div>
     <div class="relative flex justify-center items-end w-[80%] h-[80%] p-2 bg-blue-900 bg-opacity-90 rounded-3xl">
       <div class="flex justify-center items-center gap-2 w-[95%] h-[90%] z-10">
-        <Element :src="`${options[currentPage].path}`" customClass="w-full"/>
+        <Element :src="`${options[currentPage].path}`" customClass="w-full max-h-full"/>
       </div>
     </div>
   </div>
