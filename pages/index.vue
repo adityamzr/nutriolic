1e3bcd3f93643162c590a17b8ce4555d8ecbf155
Aditya Muhammad Zaini <adityamzainir@gmail.com>
2025-04-30 19:16:29 +0700

    fix(style): size adjusment index

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="relative h-screen w-full flex justify-center items-center overflow-hidden">
     <div class="w-[80%] h-[60%] p-4">
-      <!-- <Button class="mt-4" variant="primary" @click="handleButtonClick('full')">Full Screen</Button>
-      <Button class="mt-4 mx-3" variant="secondary" @click="handleButtonClick('minimize')">Minimize</Button> -->
-      <div class="flex justify-center absolute top-0 left-1/2 -translate-x-1/2">
-        <Element src="/element/nutriolic-title.svg" customClass="h-[140px] md:h-32 lg:h-48" />
-        <span class="absolute top-28 text-sm font-semibold">Pembelajaran Nutrisi Interaktif</span>
+      <div class="flex justify-center absolute top-0 md:top-10 left-1/2 -translate-x-1/2">
+        <Element src="/element/nutriolic-title.svg" customClass="h-[140px] md:h-38 lg:h-52" />
+        <span class="absolute top-28 md:top-28 lg:top-40 text-sm md:text-md lg:text-lg font-semibold">Pembelajaran Nutrisi Interaktif</span>
       </div>
-      <Element src="/element/milk.gif" customClass="absolute left-[120px] bottom-20 w-16" />
-      <Element src="/element/cereal.gif" customClass="absolute left-[140px] bottom-14 w-28" />
+      <Element src="/element/milk.gif" customClass="absolute left-[120px] bottom-20 lg:bottom-36 w-16 lg:w-24" />
+      <Element src="/element/cereal.gif" customClass="absolute left-[140px] bottom-14 lg:bottom-28 w-28 lg:w-36" />
       <div class="absolute right-2 bottom-[-60px]">
-        <ChefRight/>
+        <ChefRight class="w-40 md:w-52 lg:w-72"/>
       </div>
     </div>
-    <button @click="handleClick('/login')" class="absolute bottom-20 w-36 py-2 px-4 rounded-full bg-red-200">
-      <LilitaText class="text-stroke-2 text-4xl">Mulai</LilitaText>
+    <button @click="handleClick('/login')" class="absolute bottom-20 lg:bottom-32 w-36 lg:w-fit py-2 px-4 rounded-full bg-red-200">
+      <LilitaText class="text-stroke-2 lg:text-stroke-3 text-4xl lg:text-6xl">Mulai</LilitaText>
     </button>
   </div>
 </template>
@@ -23,16 +21,6 @@
 import { useRouter } from "vue-router";
 
 const router = useRouter();
-
-// const handleButtonClick = (type) => {
-//   const element = document.documentElement;
-
-//   if (type === "full") {
-//     (element.requestFullscreen || element.mozRequestFullScreen || elem.webkitRequestFullscreen || elem.msRequestFullscreen)?.call(element);
-//   } else {
-//     if(!!document.fullscreenElement) (document.exitFullscreen || document.mozCancelFullScreen || document.webkitExitFullscreen || document.msExitFullscreen)?.call(document);
-//   }
-// }
 
 const handleClick = (path) => {
   const element = document.documentElement;
