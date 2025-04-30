55fe299dff404974c9e93031a66cee9146e6083e
Aditya Muhammad Zaini <adityamzainir@gmail.com>
2025-04-30 19:22:53 +0700

    fix(style): size adjusment on index

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,13 @@
   <div class="relative h-screen w-full flex justify-center items-center overflow-hidden">
     <div class="w-[80%] h-[60%] p-4">
       <div class="flex justify-center absolute top-0 md:top-10 left-1/2 -translate-x-1/2">
-        <Element src="/element/nutriolic-title.svg" customClass="h-[140px] md:h-38 lg:h-52" />
+        <Element src="/element/nutriolic-title.svg" customClass="h-[140px] md:h-38 lg:h-60" />
         <span class="absolute top-28 md:top-28 lg:top-40 text-sm md:text-md lg:text-lg font-semibold">Pembelajaran Nutrisi Interaktif</span>
       </div>
-      <Element src="/element/milk.gif" customClass="absolute left-[120px] bottom-20 lg:bottom-36 w-16 lg:w-24" />
-      <Element src="/element/cereal.gif" customClass="absolute left-[140px] bottom-14 lg:bottom-28 w-28 lg:w-36" />
-      <div class="absolute right-2 bottom-[-60px]">
-        <ChefRight class="w-40 md:w-52 lg:w-72"/>
+      <Element src="/element/milk.gif" customClass="absolute left-[120px] lg:left-80 bottom-20 lg:bottom-36 w-16 lg:w-24" />
+      <Element src="/element/cereal.gif" customClass="absolute left-[140px] lg:left-80 bottom-14 lg:bottom-28 w-28 lg:w-36" />
+      <div class="absolute right-2 lg:right-10 bottom-[-60px] lg:-bottom-20">
+        <ChefRight class="w-40 md:w-52 lg:w-96"/>
       </div>
     </div>
     <button @click="handleClick('/login')" class="absolute bottom-20 lg:bottom-32 w-36 lg:w-fit py-2 px-4 rounded-full bg-red-200">
