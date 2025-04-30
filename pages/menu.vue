379c41a065f350dad34bcda727787f8dd5c480c9
Aditya Muhammad Zaini <adityamzainir@gmail.com>
2025-04-30 20:00:40 +0700

    fix(style): size adjusment on menu

--- pages/menu.vue
+++ pages/menu.vue
@@ -1,31 +1,27 @@
 <template>
   <div class="relative h-screen w-full flex justify-center items-center overflow-hidden">
-    <div class="flex justify-center items-end border-2 w-[80%] h-[60%] p-4">
-      <!-- <div class="flex justify-center absolute top-0 left-1/2 -translate-x-1/2">
-        <Element src="/element/nutriolic-title.svg" customClass="h-[140px] md:h-32 lg:h-48" />
-        <span class="absolute top-28 text-sm font-semibold">Pembelajaran Nutrisi Interaktif</span>
-      </div> -->
+    <div class="flex justify-center items-end w-[80%] h-[60%] p-4">
       <div class="flex justify-evenly items-center w-[80%] h-[50%]">
         <div class="flex flex-col items-center">
-          <CircleButton @click="handleClick('/learning-objectives')" class="w-20 md:w-28 h-20 md:h-28">
-            <Element src="/element/play.png" customClass="w-12 md:w-16"/>
+          <CircleButton @click="handleClick('/learning-objectives')" class="w-20 md:w-28 lg:w-36 h-20 md:h-28 lg:h-36">
+            <Element src="/element/play.png" customClass="w-12 md:w-16 lg:w-20"/>
           </CircleButton>
-          <span class="text-sm font-semibold">SIAP!</span>
+          <span class="text-sm lg:text-2xl font-semibold">SIAP!</span>
         </div>
         <div class="flex flex-col items-center">
-          <CircleButton @click="handleClick('/tutorial')" class="w-20 md:w-28 h-20 md:h-28">
+          <CircleButton @click="handleClick('/tutorial')" class="w-20 md:w-28 lg:w-36 h-20 md:h-28 lg:h-36">
             <Element src="/element/question-mark.png" customClass="w-10 md:w-14"/>
           </CircleButton>
-          <span class="text-sm font-semibold">Cara Penggunaan</span>
+          <span class="text-sm lg:text-2xl font-semibold">Cara Penggunaan</span>
         </div>
       </div>
-      <div class="absolute right-0 bottom-[-60px]">
-        <ChefRight/>
+      <div class="absolute right-2 lg:right-10 bottom-[-60px] lg:-bottom-20">
+        <ChefRight class="w-40 md:w-52 lg:w-96"/>
       </div>
-      <div class="absolute right-32 top-0">
-        <div class="relative items-center w-56 md:w-56">
+      <div class="absolute right-32 md:right-40 lg:right-[340px] top-0 lg:top-16">
+        <div class="relative items-center w-56 md:w-56 lg:w-72">
           <Element src="/element/cloud.png" customClass="w-[100%]"/>
-          <div class="absolute h-40 inset-0 flex items-center justify-center px-2 text-xs text-center font-semibold text-black">
+          <div class="absolute h-40 md:h-40 lg:48 inset-0 lg:inset-5 flex items-center justify-center px-2 text-xs lg:text-sm text-center font-semibold text-black">
             Halo Nutrikids! Senang sekali bisa bersama kalian. Yuk, jelajahi kebutuhan nutrisi kita! Sudah siap? Sebelum mulai, pastikan kalian tahu cara menggunakan aplikasi ini ya!
           </div>
         </div>
