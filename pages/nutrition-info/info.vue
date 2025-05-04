fd5edc81184f12cb448367181ee3f052ec9b8354
Aditya Muhammad Zaini <adityamzainir@gmail.com>
2025-05-04 21:02:23 +0700

    fix(style): size adjusment on all video and info pages

--- pages/nutrition-info/info.vue
+++ pages/nutrition-info/info.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="relative h-screen w-full flex justify-center items-center overflow-hidden">
-    <div class="h-16 w-20 absolute top-0 left-0 flex justify-evenly items-center">
-      <CircleButton @click="handleClick('/material-menu')" class="h-12 w-12" variant="secondary">
-        <Element src="/element/arrow-left.png" customClass="h-6" />
+    <div class="h-16 lg:h-24 w-20 lg:w-28 absolute top-0 left-0 flex justify-evenly items-center">
+      <CircleButton @click="handleClick('/material-menu')" class="h-12 w-12 lg:h-16 lg:w-16" variant="secondary">
+        <Element src="/element/arrow-left.png" customClass="h-6 lg:h-9" />
       </CircleButton>
     </div>
-    <div class="z-10 flex justify-center absolute top-5 left-1/2 -translate-x-1/2">
-      <LilitaText class="text-stroke-4 text-5xl min-w-96">INFORMASI GIZI</LilitaText>
+    <div class="z-10 flex justify-center absolute top-5 lg:top-10 left-1/2 -translate-x-1/2">
+      <LilitaText class="text-stroke-4 text-5xl lg:text-8xl min-w-96">INFORMASI GIZI</LilitaText>
     </div>
     <div class="relative flex justify-center items-end w-[75%] h-[75%] p-2 bg-blue-900 bg-opacity-90">
       <Element src="/element/orange.png" customClass="absolute top-[-30px] left-[-20px] w-16"/>
@@ -14,12 +14,12 @@
       <Element src="/element/watermelon.png" customClass="absolute top-[-20px] right-[-20px] w-16"/>
       <Element src="/element/grape.png" customClass="absolute bottom-[-25px] right-[-30px] w-16"/>
       <div class="flex flex-col items-center justify-start bg-rose-400 rounded-2xl p-2 w-[85%] h-[90%] z-10">
-        <LilitaText class="text-xs min-w-96 text-yellow-300">CEGAH PENYAKIT TIDAK MENULAR DENGAN RAJIN CEK INFORMASI GIZI</LilitaText>
-        <div class="overflow-auto">
-          <p class="text-white text-justify text-[10px]">BPOM menekankan pentingnya menerapkan prinsip gizi seimbang dalam kehidupan sehari-hari untuk mencegah penyakit tidak menular. Prinsip ini meliputi mengonsumsi makanan sesuai isi piringku, membaca label gizi sebelum membeli produk makanan atau minuman, serta menjaga pola makan dengan membatasi asupan gula, garam, dan lemak sesuai anjuran Kementerian Kesehatan. Selain itu, menjaga berat badan tetap normal dengan aktivitas fisik yang teratur dan menerapkan perilaku hidup bersih juga merupakan bagian dari upaya menjaga kesehatan. Dalam sebuah diskusi kesehatan tentang obesitas di Jakarta pada 4 Maret 2024, Pengawas Farmasi dan Makanan Ahli Muda BPOM Pratiwi Yuniarti Martoyo menegaskan bahwa pengawasan asupan gizi melalui label kemasan sangat penting dilakukan agar masyarakat dapat memilih makanan sesuai dengan kebutuhan gizinya.</p>
-          <p class="text-white text-justify text-[10px]">Sebagai langkah mendukung transparansi informasi gizi, BPOM telah mengatur agar produk pangan olahan dalam kemasan, termasuk yang dijual di toko online dan produk UMKM, mencantumkan tabel informasi nilai gizi. Label ini mencantumkan jumlah takaran saji per kemasan, total energi per sajian, kandungan zat gizi seperti lemak, lemak jenuh, protein, karbohidrat (termasuk gula), dan persentase Angka Kecukupan Gizi (AKG) per sajian. Masyarakat diimbau untuk lebih cermat dalam membaca label gizi agar konsumsi makanan tetap seimbang dan sesuai dengan kebutuhan tubuh.</p>
+        <LilitaText class="text-xs md:text-sm lg:text-2xl min-w-96 text-yellow-300">CEGAH PENYAKIT TIDAK MENULAR DENGAN RAJIN CEK INFORMASI GIZI</LilitaText>
+        <div class="overflow-auto p-2 lg:p-4">
+          <p class="text-white text-justify text-[10px] md:text-xs lg:text-lg">BPOM menekankan pentingnya menerapkan prinsip gizi seimbang dalam kehidupan sehari-hari untuk mencegah penyakit tidak menular. Prinsip ini meliputi mengonsumsi makanan sesuai isi piringku, membaca label gizi sebelum membeli produk makanan atau minuman, serta menjaga pola makan dengan membatasi asupan gula, garam, dan lemak sesuai anjuran Kementerian Kesehatan. Selain itu, menjaga berat badan tetap normal dengan aktivitas fisik yang teratur dan menerapkan perilaku hidup bersih juga merupakan bagian dari upaya menjaga kesehatan. Dalam sebuah diskusi kesehatan tentang obesitas di Jakarta pada 4 Maret 2024, Pengawas Farmasi dan Makanan Ahli Muda BPOM Pratiwi Yuniarti Martoyo menegaskan bahwa pengawasan asupan gizi melalui label kemasan sangat penting dilakukan agar masyarakat dapat memilih makanan sesuai dengan kebutuhan gizinya.</p>
+          <p class="text-white text-justify text-[10px] md:text-xs lg:text-lg mt-2">Sebagai langkah mendukung transparansi informasi gizi, BPOM telah mengatur agar produk pangan olahan dalam kemasan, termasuk yang dijual di toko online dan produk UMKM, mencantumkan tabel informasi nilai gizi. Label ini mencantumkan jumlah takaran saji per kemasan, total energi per sajian, kandungan zat gizi seperti lemak, lemak jenuh, protein, karbohidrat (termasuk gula), dan persentase Angka Kecukupan Gizi (AKG) per sajian. Masyarakat diimbau untuk lebih cermat dalam membaca label gizi agar konsumsi makanan tetap seimbang dan sesuai dengan kebutuhan tubuh.</p>
           <div class="flex items-end justify-center grow">
-            <Element @click="handleClick('/nutrition-info/material')" src="/element/start.png" customClass="w-20 my-2"/>
+            <Element @click="handleClick('/nutrition-info/material')" src="/element/start.png" customClass="w-20 my-2 lg:w-40 lg:my-4"/>
           </div>
         </div>
       </div>
--- pages/nutrition/video.vue
+++ pages/nutrition/video.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="relative h-screen w-full flex justify-center items-center overflow-hidden">
-    <div class="h-16 w-20 absolute top-0 left-0 flex justify-evenly items-center">
-      <CircleButton @click="handleClick('/material-menu')" class="h-12 w-12" variant="secondary">
-        <Element src="/element/arrow-left.png" customClass="h-6" />
+    <div class="h-16 lg:h-24 w-20 lg:w-28 absolute top-0 left-0 flex justify-evenly items-center">
+      <CircleButton @click="handleClick('/material-menu')" class="h-12 w-12 lg:h-16 lg:w-16" variant="secondary">
+        <Element src="/element/arrow-left.png" customClass="h-6 lg:h-9" />
       </CircleButton>
     </div>
-    <div class="z-10 flex justify-center absolute top-5 left-1/2 -translate-x-1/2">
-      <LilitaText class="text-stroke-4 text-6xl">NUTRISI</LilitaText>
+    <div class="z-10 flex justify-center absolute top-5 lg:top-12 left-1/2 -translate-x-1/2">
+      <LilitaText class="text-stroke-4 text-6xl lg:text-8xl">NUTRISI</LilitaText>
     </div>
     <div class="relative flex justify-center items-end w-[75%] h-[75%] p-2 bg-blue-900 bg-opacity-90">
       <Element src="/element/orange.png" customClass="absolute top-[-30px] left-[-20px] w-20"/>
@@ -16,24 +16,24 @@
       <div class="flex justify-center gap-2 w-[90%] h-[90%] z-10">
         <div class="flex flex-col justify-center w-[65%] md:w-[70%]">
           <Video src="/video/nutrition.mp4" class="h-auto"></Video>
-          <span class="text-white text-xs">jika Anda sudah memahami videonya, silakan klik tombol "Play" di samping untuk melanjutkan perjalanan kita!</span>
+          <span class="text-white text-xs md:text-sm lg:text-xl">jika Anda sudah memahami videonya, silakan klik tombol "Play" di samping untuk melanjutkan perjalanan kita!</span>
         </div>
         <!-- <div class="flex flex-col items-center justify-between grow"> -->
         <div class="flex items-end justify-center grow">
-          <Element @click="handleClick('/nutrition/material')" src="/element/start.png" customClass="w-20 mb-8"/>
+          <Element @click="handleClick('/nutrition/material')" src="/element/start.png" customClass="w-20 lg:w-40 mb-8 lg:mb-16"/>
         </div>
       </div>
     </div>
-    <div class="absolute w-24 md:w-20 right-[170px] md:right-56 top-20">
-      <div class="relative items-center w-44 md:w-48">
-        <Element src="/element/cloud2.png" customClass="w-[100%]"/>
-        <div class="absolute top-0 left-1/2 -translate-x-1/2 h-[120px] w-36 md:h-32 md:w-40 inset-0 flex items-center justify-center px-2 text-xs text-center font-semibold text-black">
+    <div class="absolute w-24 md:w-20 lg:w-80 right-[170px] md:right-56 top-20 lg:top-32">
+      <div class="relative items-center w-44 md:w-48 lg:w-80">
+        <Element src="/element/cloud2.png" customClass="w-full"/>
+        <div class="absolute top-0 lg:top-8 left-1/2 -translate-x-1/2 h-[120px] w-36 md:h-32 md:w-40 lg:h-40 lg:w-52 inset-0 flex items-center justify-center px-2 text-xs lg:text-lg text-center font-semibold text-black">
           Halo para siswa yang cerdas, saksikan video tentang 'pentingnya nutrisi untuk otak kita' dengan saksama!
         </div>
       </div>
     </div>
-    <div class="absolute w-32 md:w-40 right-2 bottom-0">
-      <ChefRight/>
+    <div class="absolute w-32 md:w-40 lg:w-72 right-2 bottom-0">
+      <ChefRight class="lg:w-72"/>
     </div>
   </div>
 </template>
